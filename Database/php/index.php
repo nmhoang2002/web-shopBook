10eca3167a3e2135f1f43d098f3c0dcc17d6ed5c
DaiTrang36 <daitrang3062002@gmail.com>
2021-11-16 09:29:39 +0700

    sắp xếp lại thư mục theo abc

---
+++ Database/php/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="vi">
+    <head>
+        <meta charset="utf-8">
+        <title>Giao Diên</title>
+        <link rel="stylesheet" href="../css/Styles.css"> <!-- link tới file CSS -->
+    </head>
+    <body>
+        <div id = "main"><!--bao toàn bộ khung website-->
+            <div id ="head"><!--phần Banner-->
+                <h1>MUA SÁCH NHANH VÀ RẺ NHẤT</h1>
+                <h2>TẠI SHOP ONLINE</h2>
+                <h3>HOÀNG TIẾN TRANG</h3>
+                <h4>(Những bộ sách sắp phát hành với giá ưu đãi cực sốc)</h4>
+            </div><!--End phần Banner-->
+            <div id ="menutop"><!--phần Menutop-->
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Tin dự án</a></li>
+                    <li><a href="#">Các hoạt động</a></li>
+                    <li><a href="#">Cơ cấu sách</a></li>
+                    <li><a href="#">Bình luận</a></li>
+                    <li><a href="#">Nhà xuất bản</a></li>
+                    <li><a href="#">Tài nguyên</a></li>
+                </ul>
+            </div><!--End phần Menutop-->
+            <div id ="maincontent"><!--phần bao của content-->
+               <div id="content">
+                   <div class ="dinhdang"><!--phần định dạng chung cho content và right-->
+                   </div><!--End định dạng cho nội dung website-->
+                   </div><!--End phần định dạng chung cho content và right-->
+                </div>
+            </div><!--End phần bao của content-->
+            <div id = "right"><!--phần menu bên phải của website-->
+                <div class ="dinhdang"><!--phần định dạng chung cho content và right-->
+                    <h1>Menu chính</h1>
+                    <div id= "menuright">
+                        <li><a href="#">menu</a></li>
+                    </div>
+                </div><!--End phần định dạng chung cho content và right-->
+            </div><!--End phần menu bên phải của website-->
+            <div id = "footer"><!--phần cuối trang-->
+                CopyRight: Trang bán Sách<br>
+                Biên tập thiết kế, xây dựng và quản trị: Em Phạm Văn Tiến<br>
+                Chịu trách nhiệm chính: Đỗ Thị Đài Trang, Nguyễn Minh Hoàng
+            </div><!--End phần cuối trang-->
+        </div><!--End bao toàn bộ khung website-->
+    </body>
+</html>       
