bdfa0932a4675ab2d31e1baf1621214d8591683d
DaiTrang36 <daitrang3062002@gmail.com>
2021-12-04 09:41:37 +0700

    .

--- Database/php/index.php
+++ Database/php/index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Giao Diên</title>
+        <link rel="stylesheet" href="../js/scrip.js">
         <link rel="stylesheet" href="../css/Styles.css"> <!-- link tới file CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -35,13 +36,14 @@
                     <li><a href="#">Thể Loại</a></li>
                     <li><a href="#">Các hoạt động</a></li>
                     <li><a href="#">Trợ giúp</a></li>
-                    <li><a href="#">Nhà xuất bản</a></li>
-                    <!-- icons giỏ hàng -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16" >
+                    <li><a href="#">Nhà xuất bản</a></li> 
+                    <!--  icons giỏ hàng -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" float="left"fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16" >
   <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
 </svg>                
                 </ul>
             </div><!--End phần Menutop-->
+            
             <div id ="maincontent"><!--phần bao của content-->
             <div class="album py-5 bg-light">
           <div class="container">
